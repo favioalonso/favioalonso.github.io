5e4c919e2fe4f2acee3cab0a6e2747af950ec2f0
favioalonso <125057409+favioalonso@users.noreply.github.com>
2023-02-10 12:46:58 -0300

    Initial commit

---
+++ funciones.js
@@ -0,0 +1,34 @@
+document.getElementById('boton_contacto').addEventListener('click', function() {
+    document.getElementById('parrafo_contacto').style.display = 'block';
+
+});
+
+document.getElementById('boton_idiomas').addEventListener('click', function() {
+    document.getElementById('parrafo_idiomas').style.display = 'block';
+
+});
+
+document.getElementById('boton_aptitudes').addEventListener('click', function() {
+    document.getElementById('parrafo_aptitudes').style.display = 'block';
+
+});
+
+document.getElementById('boton_otros').addEventListener('click', function() {
+    document.getElementById('parrafo_otros').style.display = 'block';
+
+});
+
+document.getElementById('boton_perfil').addEventListener('click', function() {
+    document.getElementById('parrafo_perfil').style.display = 'block';
+
+});
+
+document.getElementById('boton_exp').addEventListener('click', function() {
+    document.getElementById('parrafo_exp').style.display = 'block';
+
+});
+
+document.getElementById('boton_formacion').addEventListener('click', function() {
+    document.getElementById('parrafo_formacion').style.display = 'block';
+
+});
